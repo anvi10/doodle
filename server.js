2197eace24ad9277a14cc639b4e3e5e386777512
anvi10 <arayabha@purdue.edu>
2016-11-26 12:00:09 -0800

    worked on login, integrated passport

--- server.js
+++ server.js
@@ -5,7 +5,12 @@ var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var cookieparse = require('cookie-parser');
 var expresshb = require('express-handlebars');
+var passport = require('passport');
+var passlocal = require('passport-local').Strategy;
+var expressSesh = require('express-session');
 mongoose.connect("mongodb://localhost/doodle");
+
+app.use(express.static(__dirname+'/views/static'));
 
 //setting up function to render a view with an object
 //associate all the handlebars files with the view engine
@@ -17,7 +22,61 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(cookieparse());
  //anytime you have cookies it will decode them for us
-app.use(express.static(__dirname+'/views/static'));
+
+app.use(expressSesh({
+    secret: 'password',
+    resave: false,
+    saveUninitialized: true
+    
+} ));
+
+passport.serializeUser(function(userobj, done) {
+    done(null, userobj.username);
+} );
+
+passport.deserializeUser(function(username, done) {
+    User.findOne({username: username}, function(err, user){
+        if(err) {
+            done(err);
+            return; 
+        }
+        
+        if(!user) { //user DNE. tell passport that theres no err from DB, but user just DNE
+            done(null, false);
+        } else {
+            //no err, and the username exists in the database, so we wanna seriralize it. give the usr obj
+            done(null, user);
+        }
+        
+    });
+} );
+
+passport.use('local', new passlocal(function( username, password, done) {
+    //make sure the user exists
+    //make sure that the password matches with what that users password is
+    User.findOne( {username:username}, function(err, user) {
+        if (err) {
+            done(err);
+            return;
+        }
+        
+        if (!user) {
+            done(null, false);
+        } else {
+            if ( user.verifyPassword(password) ) {
+                //match
+                done (null, user);
+            } else {
+                done(null, false);
+            }
+        }
+        
+    } );
+    
+} ) );
+
+app.use(passport.initialize() );
+app.use(passport.session() );
 
 //function that respond to home page
 app.get('/', function(req, res) {
